a2ba8cd1ace72e63334789f55eae840b03737a6a
Bhabani Pujari <pujaribhabani369@gmail.com>
2025-12-11 12:16:11 +0530

    Switch to SQLite database

--- Backend2/db.py
+++ Backend2/db.py
@@ -3,19 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Use SQLite for deployment
+DATABASE_URL = "sqlite:///./healthtrack.db"
 
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT", 3306)
-MYSQL_DB = os.getenv("MYSQL_DB")
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,  # Set to False to reduce log noise in production
+    connect_args={"check_same_thread": False}
+)
 
-DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
-
-engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
